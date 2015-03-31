2f5f2e557d1b90cb6ec920f387352818ed305368
Matthew Findlater <matthew.findlater@gmail.com>
2015-03-31 11:55:10 -0400

    Update main.js
    just fixed some formatting.

--- cloud/main.js
+++ cloud/main.js
@@ -1,25 +1,24 @@
 Parse.Cloud.beforeSave("Image", function(request, response) {
-	for (var i = 0; i<request.object.get('pixelation');i++)
-	{
-		var PixelImage = Parse.Object.extend('PixelImage');
-		var pixelImage = new PixelImage();
-		pixelImage.set('image',request.object.get('image'));
-		
-		pixelImage.save().then(
-  		function(savedPixelImage) {
-    		request.object.add('pixelImages',{"__type":"Pointer","className":"PixelImage","objectId":savedPixelImage.id});
-             console.log('Successfully created pixelImage.');
-             if (i = i<request.object.get('pixelation')-1)
-             {
-             	console.log("Before Save: " + request.object.get('image'));
-				response.success();
-             }
-  		},
-  		function(error) {
-    		console.log("Could not save a pixelImage " + error.code + ' : ' +error.message);
-  		});
+    for (var i = 0; i < request.object.get('pixelation'); i++) {
+        var PixelImage = Parse.Object.extend('PixelImage');
+        var pixelImage = new PixelImage();
+        pixelImage.set('image', request.object.get('image'));
 
-	}
-  	
-  
+        pixelImage.save().then(
+            function(savedPixelImage) {
+                request.object.add('pixelImages', {
+                    "__type": "Pointer",
+                    "className": "PixelImage",
+                    "objectId": savedPixelImage.id
+                });
+                console.log('Successfully created pixelImage.');
+                if (i = i < request.object.get('pixelation') - 1) {
+                    console.log("Before Save: " + request.object.get('image'));
+                    response.success();
+                }
+            },
+            function(error) {
+                console.log("Could not save a pixelImage " + error.code + ' : ' + error.message);
+            });
+    }
 });
